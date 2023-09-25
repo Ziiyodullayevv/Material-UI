c611475c1cef99307d336e6560665e77a2b1d49f
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-09-25 09:29:17 +0500

    Mui Table

--- src/components/MuiTable/index.jsx
+++ src/components/MuiTable/index.jsx
@@ -38,6 +38,32 @@ const MuiTable = () => {
           </TableBody>
         </Table>
       </TableContainer>
+
+      <TableContainer component={Paper} sx={{ maxHeight: "300px" }}>
+        <Table aria-label="simple table" stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              <TableCell>Ism</TableCell>
+              <TableCell>Familiya</TableCell>
+              <TableCell align="center">Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableData.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child  th": { border: 0 } }}
+              >
+                <TableCell>{row.id}</TableCell>
+                <TableCell>{row.first_name}</TableCell>
+                <TableCell>{row.last_name}</TableCell>
+                <TableCell align="center">{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
