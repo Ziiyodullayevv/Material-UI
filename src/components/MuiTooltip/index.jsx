0e1ccb0127a53cfc78d5a2bfe54966ba53b44da7
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-09-27 09:26:34 +0500

    Mui Tooltip

--- src/components/MuiTooltip/index.jsx
+++ src/components/MuiTooltip/index.jsx
@@ -17,6 +17,19 @@ const MuiTooltip = () => {
           </IconButton>
         </Tooltip>
       </Stack>
+
+      <Stack sx={{ margin: 5 }} direction={"row"} spacing={3}>
+        <Tooltip enterDelay={500} leaveDelay={200} title={"Delete Button"}>
+          <IconButton>
+            <Delete />
+          </IconButton>
+        </Tooltip>
+        <Tooltip arrow title={"Delete Button"}>
+          <IconButton>
+            <Delete />
+          </IconButton>
+        </Tooltip>
+      </Stack>
     </React.Fragment>
   );
 };
